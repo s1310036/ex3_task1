4c2a48ab82a4e8eff904a4c3ed3634740f1cb00b
s1310036 <s1310036@u-aizu.ac.jp>
2024-06-28 23:32:15 +0900

    User name added

--- coin.c
+++ coin.c
@@ -2,28 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-const char* coin_toss(){
-  return rand()%2==0 ? "Heads":"Tails";
+const char* coin_toss() {
+    return rand() % 2 == 0 ? "Heads" : "Tails";
 }
 
-int main(){
-  int heads_count=0;
-  int tails_count=0;
+int main() {
+    char name[50];
+    int heads_count = 0;
+    int tails_count = 0;
 
-  srand(time(NULL));
+    srand(time(NULL));
 
-  printf("Tossing a coin...\n");
-  for(int i=1;i<=3;i++){
-    const char* result=coin_toss();
-    printf("Round %d: %s\n", i,result);
-    if(result=="Heads"){
-      heads_count++;
-    }else{
-      tails_count++;
+    printf("Who are you?\n> ");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+
+    printf("Tossing a coin...\n");
+    for (int round = 1; round <= 3; round++) {
+        const char* result = coin_toss();
+        printf("Round %d: %s\n", round, result);
+        if (result == "Heads") {
+            heads_count++;
+        } else {
+            tails_count++;
+        }
     }
-  }
-  printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
-  return 0;
-}
 
-    
+    printf("Heads: %d, Tails: %d\n", heads_count, tails_count);
+    return 0;
+}
